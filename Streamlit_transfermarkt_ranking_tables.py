5710d0aeb121658f4440498597c24598a54ec428
Frederic Girod <fred.fredericgirod@gmail.com>
2021-03-26 21:22:39 +0100

    Update Streamlit_transfermarkt_ranking_tables.py

--- Streamlit_transfermarkt_ranking_tables.py
+++ Streamlit_transfermarkt_ranking_tables.py
@@ -12,6 +12,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from PIL import Image
+import datetime
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"}
 
